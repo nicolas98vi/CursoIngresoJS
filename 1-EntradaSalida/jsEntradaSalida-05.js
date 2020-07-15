1a50d70684594160971b84693114079903f73ffa
nicolas98vi <nicrost98@hotmail.com>
2020-07-15 10:00:19 -0300

    cambio

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -3,6 +3,11 @@ Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	alert("ok");
-}
+	var nombre;
+	var edad;
+	    nombre = txtIdNombre.value;
+		edad = txtIdEdad.value;
+        alert("usted se llama" + nombre + "y tiene" + edad);
+
+	}
 
--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,16 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	var uno ;
+	var dos ;
+	var numero;
+		uno = txtIdNumeroUno . value;
+		dos = txtIdNumeroDos . value;
+		uno = parseInt(uno);
+		dos = parseInt(dos);
+		numero = uno + dos;
+
+		alert ("la suma es: " + numero);
+
 }
 
